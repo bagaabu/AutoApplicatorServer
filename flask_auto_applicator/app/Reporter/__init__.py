1917adab948732896b0614a0c51cf5aca67286e9
zhanglei12 <zhanglei12@baijia.com>
2023-03-25 18:16:10 +0800

    change to my db

--- flask_auto_applicator/app/Reporter/__init__.py
+++ flask_auto_applicator/app/Reporter/__init__.py
@@ -3,7 +3,7 @@ from app.Reporter.const import stage_dict
 
 user = 'abu'
 password = 'abu123'
-address = '10.252.73.99'
+address = '121.41.58.52'
 Recoder = dbReporter(user, password, address)
 Recoder.setup_db('NX-database', 'personal-info', 1)
 
