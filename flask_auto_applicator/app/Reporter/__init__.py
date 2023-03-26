98ffbaf68507d36523fbad7525e824bdc6e6cb9b
zhanglei12 <zhanglei12@baijia.com>
2023-03-26 19:23:43 +0800

    monogodb error check

--- flask_auto_applicator/app/Reporter/__init__.py
+++ flask_auto_applicator/app/Reporter/__init__.py
@@ -1,8 +1,8 @@
 from app.Reporter.infor import dbReporter
 from app.Reporter.const import stage_dict
 
-user = 'abu'
-password = 'abu123'
+user = 'admin'
+password = 'admin123'
 address = '121.41.58.52'
 Recoder = dbReporter(user, password, address)
 Recoder.setup_db('NX-database', 'personal-info', 1)
