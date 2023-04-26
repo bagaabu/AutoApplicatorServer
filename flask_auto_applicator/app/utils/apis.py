2cf2152554a7264fe3210bf186f78c06b71fa939
zhanglei12 <zhanglei12@baijia.com>
2023-04-26 18:04:51 +0800

    fix api bug

--- flask_auto_applicator/app/utils/apis.py
+++ flask_auto_applicator/app/utils/apis.py
@@ -5,7 +5,8 @@ import json
 
 def send_request(target, data, request_id):
     headers = {'Content-Type': 'application/json'}
-    request_data = {'request_id': request_id, 'data': data}
+    # request_data = {'request_id': request_id, 'data': data}
+    request_data = data
     response = requests.post(urls[target], headers=headers, data=json.dumps(request_data), timeout=800)
     res_data = json.loads(response.text)
     return res_data
